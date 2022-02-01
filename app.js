c0f1de4c0f54eeb8034b137211166a665eaaf8f1
malak-lawanseh <malaklawanseh0@gmail.com>
2022-02-01 21:11:16 +0200

    adding rubberband animation

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let content=document.querySelector(".content")
+    document.querySelector('#about').onclick=function(){
+        
+    }
+        
+
+    
+    
+});
